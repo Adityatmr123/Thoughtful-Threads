d44a4777410da0383232b1396f1ba298cf473499
ADITYA SINGH TOMAR <96469224+Adityatmr123@users.noreply.github.com>
2024-08-28 23:45:22 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js'
+import path from 'path';
 import cookieParser from "cookie-parser";
 import postRoutes from './routes/post.route.js'
 import commentRoutes from './routes/comment.route.js'
-import path from 'path';
+
 
 dotenv.config();
 
